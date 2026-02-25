03b88586d17df085e69847aea449fa8b97ca46ca
remo12262 <54858877+remo12262@users.noreply.github.com>
2026-02-25 21:13:29 +0100

    Add CORS headers to server response

--- server.js
+++ server.js
@@ -15,3 +15,10 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Listening on port " + PORT);
 });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://www.quantumhorizon.it");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
